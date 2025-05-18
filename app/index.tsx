007016eef241b3afe42a99c107724c888de9727c
Marco <marco.dmail96@gmail.com>
2025-05-18 10:37:45 +0200

    Create index.tsx and import App in it

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import App from "@/App";
+import React from "react";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <App />
+    </>
   );
 }
